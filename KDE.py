96603ba6c544a019ab0c87750fc853920b765ac6
Ankith Mohan <ankithmo@usc.edu>
2020-09-08 15:08:04 -0700

    Updated sampling and KDE

---
+++ KDE.py
@@ -0,0 +1,89 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import DataLoader
+
+from sklearn.neighbors import KernelDensity
+from sklearn.model_selection import GridSearchCV
+
+
+class KDE(object):
+    """
+        Class of Kernel Density Estimation models. Modified version of
+        https://github.com/lukasruff/Deep-SAD-PyTorch/blob/master/src/baselines/kde.py
+    """
+
+    def __init__(self, kernel='gaussian', n_jobs=-1, seed=None, **kwargs):
+        """
+            Initialize KDE instance
+
+            Args:
+                - kernel: Distribution of the kernel
+                - n_jobs (int): Number of jobs
+                - seed (int): Seed for reproducibility
+        """
+        self.kernel = kernel
+        self.n_jobs = n_jobs
+        self.seed = seed
+
+        self.model = KernelDensity(kernel=kernel, **kwargs)
+        self.bandwidth = None
+
+
+    def train(self, dataset, device, batch_size=128): #n_jobs_dataloader,
+        """
+            Finds best bandwidth and fits KDE model on dataset
+
+            Args:
+                - dataset (num_datapoints, num_features): Training dataset
+                - device: Torch device
+                - batch_size (int): Batch size
+        """
+
+        # do not drop last batch for non-SGD optimization shallow_ssad
+        train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+
+        # Get data from loader
+        X = ()
+        for data in train_loader:
+            inputs = data.to(device)
+            X_batch = inputs.view(inputs.size(0), -1)  # X_batch.shape = (batch_size, n_channels * height * width)
+            X += (X_batch.cpu().data.numpy(),)
+        X = np.concatenate(X)
+
+        # Training
+        if self.bandwidth is None:
+            print("Searching over bandwidths...", end="")
+            # use grid search cross-validation to select bandwidth
+            params = {'bandwidth': np.logspace(0.5, 5, num=10, base=2)}
+            hyper_kde = GridSearchCV(KernelDensity(kernel=self.kernel), params, 
+                                     n_jobs=self.n_jobs, cv=5, verbose=0)
+            print("Done")
+
+            print("Fitting data...", end="")
+            hyper_kde.fit(X)
+            self.bandwidth = hyper_kde.best_estimator_.bandwidth
+            self.model = hyper_kde.best_estimator_
+            print("Done")
+        else:
+            self.model = KernelDensity(kernel=self.kernel, bandwidth=self.bandwidth)
+            self.model.fit(X)
+
+
+    def log_likelihood(self, samples, device):
+        """
+            Returns the log likelihood of samples
+
+            Args:
+                - samples (num_samples, num_features): Samples
+                - device: Torch device
+
+            Returns:
+                - log likelihood of samples
+        """
+        log_g = self.model.score_samples(samples.cpu().data.numpy())
+        log_g = log_g.reshape(log_g.shape[0],1)
+        log_g = torch.from_numpy(log_g).to(device)
+        return log_g
--- get_samples.py
+++ get_samples.py
@@ -20,27 +20,29 @@ def get_samples(model, x, edge_index=None, dataset=None):
     """
     Us, Cs = {}, {}
 
+    assert dataset in ["train", "val", "test"], 
+        ValueError(f"Expected `dataset` to be in [train, val, test], got {dataset} instead.")
+
+    total = x.size(0) if dataset is None else x[dataset].size(0)
+
     model.eval()
     with torch.no_grad():
         print("Unconditional sampling...")
-        _, uS = model(x) if edge_index is not None else model(x, edge_index) 
+        _, uS = model(x) if edge_index is None else model(x, edge_index) 
         for i in range(len(uS)):
             Us[i] = uS[i] if dataset is None else uS[i][dataset]
         print("Done")
-
-        total = x.size(0) if dataset is None else x[dataset].size(0)
 
         print("Conditional sampling...")
         with tqdm(total=total) as pbar:
             for j in range(total):
                 pbar.set_description(f"Conditional samples given x_{j}")
-                _, cS = model(x, edge_index, emb, layer)
+                _, cS = model(x) if edge_index is None else model(x, edge_index)
                 for i in range(len(cS)):
                   Cs[i] = cS[i] if dataset is None else cS[i][dataset]
             pbar.update(1)
         pbar.close()
-
-        for i in range(len(S)):
+        for i in range(len(Cs)):
             Cs[i] = torch.stack(Cs[i], axis=1)
             assert Cs[i].size() == (total, total, Cs[i].size(1))
         print("Done")
